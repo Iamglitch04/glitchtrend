31832e2e0f9b15b5381c5fc75fe46945a2c4d55b
iamglitch04 <glitch0414@gmail.com>
2026-03-08 11:38:33 +0530

    added admin product page

--- glitchtrend-frontend/src/App.jsx
+++ glitchtrend-frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Cart from "./pages/Cart";
-import Orders from "./pages/Orders"; // ✅ added
+import Orders from "./pages/Orders";
+import Admin from "./pages/Admin";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/orders" element={<Orders />} /> {/* ✅ added */}
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </>
   );
---
+++ glitchtrend-frontend/src/pages/Admin.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+function Admin() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+
+  const addProduct = async () => {
+    const res = await fetch(
+      "https://glitchtrend-backend.onrender.com/api/products",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          price,
+          description,
+          image,
+        }),
+      }
+    );
+
+    const data = await res.json();
+    alert("Product Added!");
+
+    console.log(data);
+  };
+
+  return (
+    <div style={{ padding: "60px" }}>
+      <h1>Add Product</h1>
+
+      <input
+        placeholder="Product Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        placeholder="Price"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <input
+        placeholder="Image URL"
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <textarea
+        placeholder="Description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={addProduct}>Add Product</button>
+    </div>
+  );
+}
+
+export default Admin;
